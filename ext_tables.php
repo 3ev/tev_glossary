0c12f6c6a9040e347afceb8d654a370202b52049
Ben Constable <ben@benconstable.co.uk>
2015-08-03 11:24:10 +0100

    Add wizicon

--- ext_tables.php
+++ ext_tables.php
@@ -21,3 +21,11 @@ if (!defined('TYPO3_MODE')) {
     'Glossary Browser Plugin',
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('tev_glossary') . 'ext_icon.png'
 );
+
+// Register the wizard icon for the glossary browser plugin
+
+if (TYPO3_MODE === 'BE') {
+    $TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['Tev\\TevGlossary\\Utility\\WizIcon'] =
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Utility/WizIcon.php';
+}
+
